c6797b7e353de0609b0b2c8fc31c4b6cd3181fd3
beast <beast@woozi>
2024-09-25 13:14:49 +0900

    base

---
+++ vitabuddy/src/main/java/com/example/vitabuddy/VitabuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitabuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitabuddyApplication.class, args);
+	}
+
+}
